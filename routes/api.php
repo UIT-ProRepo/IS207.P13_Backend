9144746e4b6f60a6336ff5b8161baf25f6e5a6b1
Nhat-Original <nhatoriginal@gmail.com>
2024-10-23 06:37:14 +0000

    Added documentations

--- routes/api.php
+++ routes/api.php
@@ -1,43 +1,42 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 
 /**
- * 
- * Public routes
- * 
- */
-
-/**
- * Auth routes
+ * ---
+ * Các route public (không cần đăng nhập)
+ * ---
  */
 Route::middleware([])->group(function () {
     Route::prefix('auth')->group(function () {
         Route::post('/signup', [AuthController::class, 'signup']);
         Route::post('/signin', [AuthController::class, 'signin']);
     });
+
+    /* Thêm các route khác ở đây */
 });
 
-
 /**
- * 
- * Protected routes
- * 
+ * ---
+ * Các route protected (cần đăng nhập)
+ * ---
  */
 
 /**
- * All roles 
+ * Tất cả các role đều có thể truy cập
  */
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/me', function (Request $request) {
         return $request->user();
     });
+
+    /* Thêm các route khác ở đây */
 });
 
 /**
- * Admin
+ * Role admin có thể truy cập
  */
 Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
     Route::get('/admin/welcome', function (Request $request) {
@@ -45,26 +44,32 @@ Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
             'message' => 'Admin route',
         ]);
     });
+
+    /* Thêm các route khác ở đây */
 });
 
 /**
- * Admin | Owner
+ * Role owner có thể truy cập
  */
-Route::middleware(['auth:sanctum', 'role:admin,owner'])->group(function () {
+Route::middleware(['auth:sanctum', 'role:owner'])->group(function () {
     Route::get('/owner/welcome', function (Request $request) {
         return response()->json([
             'message' => 'Owner route',
         ]);
     });
+
+    /* Thêm các route khác ở đây */
 });
 
 /**
- * Admin | Owner | Customer
+ * Role customer có thể truy cập
  */
-Route::middleware(['auth:sanctum', 'role:admin,owner,customer'])->group(function () {
+Route::middleware(['auth:sanctum', 'role:customer'])->group(function () {
     Route::get('/customer/welcome', function (Request $request) {
         return response()->json([
             'message' => 'Customer route',
         ]);
     });
+
+    /* Thêm các route khác ở đây */
 });
